0e9fa4ac972e6ba96585a3c31cd90ea81091cf40
Alanoud-k <Alanoudkhalid987@gmail.com>
2024-11-08 14:45:35 +0300

    Index class

---
+++ Index.java
@@ -0,0 +1,33 @@
+public class Index {
+
+    LinkedList<Document> allDocs;
+
+    public Index(){
+
+        allDocs = new LinkedList<Document>();
+    }
+
+    public void addDoc(Document doc){
+
+        allDocs.insert(doc);
+    }
+
+    public void displayDocs(){
+
+        if (allDocs.empty())
+            return;
+
+        allDocs.findfirst();
+        while (! allDocs.last()){
+            Document doc = allDocs.retrieve();
+            System.out.println("ID: "+ doc.id);
+            doc.words.display();//*******************************
+            allDocs.findnext();
+
+        }
+        //for last one
+        Document doc = allDocs.retrieve();
+        System.out.println("ID: "+ doc.id);
+        doc.words.display();//*******************************
+    }
+}
