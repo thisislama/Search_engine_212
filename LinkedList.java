4f5bfc4a5f918ccca4509918673d82918a5a62fa
rurueh1234 <ruba.emadd@gmail.com>
2024-11-10 13:30:21 +0300

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,68 +1,47 @@
+class Node<T> {
+    public T data;
+    public Node<T> next;
 
+    public Node(T val) {
+        data = val;
+        next = null;
+    }
+}
 
-    class Node<T> {
-        public T data;
-        public Node<T> next;
+public class LinkedList<T> {
+    private Node<T> head;
+    private Node<T> current;
 
-        public Node (T val) {
-            data = val;
-            next = null;
-        }
-
+    public LinkedList() {
+        head = current = null;
     }
 
-    public class LinkedList<T> {
+    public boolean empty() {
+        return head == null;
+    }
 
-        private Node<T> head;
-        private Node<T> current;
-        public LinkedList () {
-            head = current = null;
-        }
-        public boolean empty () {
-            return head == null;
-        }
-        public boolean last () {
-            return current.next == null;
-        }
-        public boolean full () {
-            return false; }
+    public boolean last() {
+        return current == null || current.next == null;
+    }
 
-        public void findfirst () {
-            current = head;
-        }
-        public void findnext () {
-            current = current.next; }
-        public T retrieve () {
-            return current.data; }
-        public void update (T val) {
-            current.data = val; }
-        public void insert (T val) {
-            Node<T> tmp;
-            if (empty()) {
-                current = head = new Node<T> (val);
-            }
-            else {
-                tmp = current.next;
-                current.next = new Node<T> (val);
-                current = current.next;
-                current.next = tmp;
-            }
-        }
-        public void remove () {
-            if (current == head) {
-                head = head.next;
-            }
-            else {
-                Node<T> tmp = head;
-                while (tmp.next != current)
-                    tmp = tmp.next;
-                tmp.next = current.next;
-            }
-            if (current.next == null)
-                current = head;
-            else
-                current = current.next;
+    public void findFirst() {
+        current = head;
+    }
+
+    public void findNext() {
+        if (current != null) {
+            current = current.next;
         }
     }
 
+    public T retrieve() {
+        return current != null ? current.data : null;
+    }
 
+    public void insert(T val) {
+        Node<T> newNode = new Node<>(val);
+        newNode.next = head;
+        head = newNode;
+        current = head;
+    }
+}
