0ef838e12f7d049d756d258eaa3f575dd2e68822
rurueh1234 <ruba.emadd@gmail.com>
2024-11-10 13:30:51 +0300

    Update Word.java

--- Word.java
+++ Word.java
@@ -1,28 +1,27 @@
-public class Word{
-  String text;
-  LinkedList<Integer> doc_IDS;
-  public Word(String w){
-    text=w;
-    doc_IDS=new LinkedList<Integer>();
-  }
-  public void add_Id(int id){
-    if (!existsIn_doc_IDS(id))
-      doc_IDS.insert(id);
-  }
-  public boolean existstsIn_doc_IDS(Integer id){
-    if(doc_IDS.empty())
-      return false;
-    doc_IDS.findFirst();
-    while (!doc_IDS.last()){
-      if (doc_IDS.retrieve().equals(id)){
-        return true;
-    }
-      doc_IDS.findNext();
-  }
-    if(doc_IDS.retrieve().equals(id)){
-      return true;
-    }
-    return false;
+public class Word {
+    String text;
+    LinkedList<Integer> doc_IDS;
 
-  }
+    public Word(String w) {
+        text = w;
+        doc_IDS = new LinkedList<>();
+    }
+
+    public void add_Id(int id) {
+        if (!existsIn_doc_IDS(id))
+            doc_IDS.insert(id);
+    }
+
+    public boolean existsIn_doc_IDS(Integer id) {
+        if (doc_IDS.empty())
+            return false;
+        doc_IDS.findFirst();
+        while (!doc_IDS.last()) {
+            if (doc_IDS.retrieve().equals(id)) {
+                return true;
+            }
+            doc_IDS.findNext();
+        }
+        return doc_IDS.retrieve().equals(id);
+    }
 }
