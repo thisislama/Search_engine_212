52a04b8213886579da3d712324d9beee0abf6c71
Alanoud-k <Alanoudkhalid987@gmail.com>
2024-11-20 00:53:04 +0300

    Update Document.java

--- Document.java
+++ Document.java
@@ -1,10 +1,13 @@
+
 public class Document{
   LinkedList<String>words=new LinkedList<>();
   int id;
+  String content;
 
-  Document(int id,LinkedList<String>words){
+  public Document(int id,LinkedList<String>words , String content){
   this.id=id;
 this.words=words;
+this.content = content;
   }
 
 
