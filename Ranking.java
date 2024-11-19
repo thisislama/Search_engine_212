02f96cae4cb7ed753512ee4120288f6aa546856f
Alanoud-k <Alanoudkhalid987@gmail.com>
2024-11-19 20:34:18 +0300

    Update Ranking.java

--- Ranking.java
+++ Ranking.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.csc212project;
 
-/**
- *
- * @author 96650
- */
 class Doc_Rank{
 int id;
 int rank;
