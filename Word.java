2b8de809ec9cc89707656f7f93716e6dad81bc1e
Alanoud-k <Alanoudkhalid987@gmail.com>
2024-11-20 00:55:07 +0300

    Update Word.java

--- Word.java
+++ Word.java
@@ -1,34 +1,90 @@
-public class Word {
-    String text;
-    LinkedList<Integer> doc_IDS;
 
-    public Word(String w) {
-        text = w;
-        doc_IDS = new LinkedList<>();
+class Node<T> {
+    public T data;
+    public Node<T> next;
+
+    public Node(T val) {
+        data = val;
+        next = null;
+    }
+}
+
+public class LinkedList<T> {
+    private Node<T> head;
+    private Node<T> current;
+    int n =0;
+
+    public LinkedList() {
+        head = current = null;
     }
 
-    public void add_Id(int id) {
-        if (!existsIn_doc_IDS(id))
-            doc_IDS.insert(id);
+    public boolean isEmpty() {
+        return head == null;
     }
 
-    public boolean existsIn_doc_IDS(Integer id) {
-        if (doc_IDS.empty())
-            return false;
-        doc_IDS.findFirst();
-        while (!doc_IDS.last()) {
-            if (doc_IDS.retrieve().equals(id)) {
-                return true;
-            }
-            doc_IDS.findNext();
+    public boolean last() {
+        return current == null || current.next == null;
+    }
+
+    public void findFirst() {
+        current = head;
+    }
+
+    public void findNext() {
+        if (current != null) {
+            current = current.next;
         }
-        return doc_IDS.retrieve().equals(id);
     }
 
+    public T retrieve() {
+        return current != null ? current.data : null;
+    }
+    
+    public void update (T val){
+        current.data = val;
+    }
+
+    public void insert(T val) {
+        n++ ;
+        Node<T> tmp; 
+        if (isEmpty())
+            current = head = new Node<T>(val);
+        else{
+           tmp = current.next;
+           current.next = new Node<T>(val);
+           current = current.next;
+           current.next = tmp;
+        }
+       
+    }
+    
+    
     public void display() {
-        System.out.println("\n------------");
-        System.out.println("Word: "+text+"\n[");
-        doc_IDS.display();
-        System.out.println("]");
+        if (this == null){
+            System.out.println("The list is null.");
+            return;
+        }
+        if (head == null) {
+            System.out.println("The list is empty.");
+            return;
+        }
+
+        Node<T> p = head;
+        while (p != null) {
+            System.out.print(p.data + " -> ");
+            p = p.next;
+        }
+    }
+    
+    public boolean exist(T x){
+        
+        Node <T> p = head;
+        while (p != null){
+            if (p.data.equals(x))
+                return true;
+            p = p.next;
+            
+        }
+        return false;
     }
 }
