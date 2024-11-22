db68befdcbbe7d728dbd9617ab0ba6fb508b56cf
Alanoud-k <Alanoudkhalid987@gmail.com>
2024-11-22 17:51:49 +0300

    Update Inverted.java

--- Inverted.java
+++ Inverted.java
@@ -1,4 +1,7 @@
 
+//linked list of linked list
+//linked list of words --> linked list of doc ids
+//retreive InvertList gives Word can use .text .doc_IDS , any method in Word
 public class Inverted {
     LinkedList<Word> InvertList;
 
@@ -6,21 +9,25 @@ public class Inverted {
         InvertList = new LinkedList<Word>();
     }
 
+    //creates inverted index: 
+    //if it doesn't exist in InverList
+    //creates Word obj (w) and inserts the id to its (doc_IDS)
+    //inserts (w) the new word to InvertList
     public void add(String text , int Id) {
         if(!Search_InvertedList(text)){ //if the text is not on the list
             Word w = new Word(text);
             w.doc_IDS.insert(Id);
             InvertList.insert(w);
         }
-        else {// if the text already on the list
-            Word exisit_word = InvertList.retrieve();// return word
-            exisit_word.doc_IDS.insert(Id);
+        else {// if the word already on the list
+            Word exisit_word = InvertList.retrieve();// return Word
+            exisit_word.add_Id(Id);// adds id to the retrieved word doc_IDS, add_Id?
         }
     }//method add end
 
 
 
-
+// search word(text) in inverted index that returns Word obj when retreived
     public boolean Search_InvertedList(String w ) {
         if(InvertList == null || InvertList.isEmpty())
           return false;
@@ -49,7 +56,7 @@ public class Inverted {
         }
         InvertList.findFirst();
         while(!InvertList.last()){
-            InvertList.retrieve().display();
+            InvertList.retrieve().display();//display in Word class
             InvertList.findNext();
         }//loop while ends
 
@@ -57,8 +64,3 @@ public class Inverted {
 //
     }
 }
-
-
-
-
-
