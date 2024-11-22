3b22a442dd3874427f4ccf2801531731c5b5764b
Alanoud-k <Alanoudkhalid987@gmail.com>
2024-11-22 17:48:13 +0300

    Update Document.java

--- Document.java
+++ Document.java
@@ -4,11 +4,15 @@ public class Document{
   int id;
   String content;
 
+  //mainly for index
+  //document with id and linked list of words except stop words
   public Document(int id,LinkedList<String>words , String content){
   this.id=id;
 this.words=words;
 this.content = content;
   }
+  
+  }
 
 
-}
+
