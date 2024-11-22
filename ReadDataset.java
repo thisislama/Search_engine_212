3833db09ac280554fee0a7293453115266ddd789
Alanoud-k <Alanoudkhalid987@gmail.com>
2024-11-22 17:55:15 +0300

    Update ReadDataset.java

--- ReadDataset.java
+++ ReadDataset.java
@@ -1,12 +1,14 @@
+
 import java.util.Scanner;
 import java.io.*;
 
 public class ReadDataset {
 
     public static void main(String [] args){
-
-        Load("\\csc212\\dataset.csv");
-
+        //changed
+        Load("C:\\Users\\96650\\Downloads\\dataset.csv");
+        //Load("C:\\Users\\96650\\Downloads\\dataset2.csv");
+        Load("C:\\Users\\96650\\Downloads\\stop.txt");
     }
 
     public static void Load(String filename){
@@ -16,18 +18,21 @@ public class ReadDataset {
 
             File f = new File(filename);
             Scanner sc = new Scanner(f);
-
+            //skip first line
             sc.nextLine();
             while (sc.hasNextLine()){
 
-                line = sc.nextLine();
+                line = sc.nextLine(); 
                 if (line.trim().length()< 3){
+                    System.out.println("Empty line, skipped");
                     break;
                 }
                 System.out.println(line);
+                
+                //splitting id and content
                 String id = line.substring(0, line.indexOf(','));
-                int idd = Integer.parseInt(id.trim());
-                String sentence = line.substring(line.indexOf(',') + 1).trim();
+                int idd = Integer.parseInt(id.trim());//convert to int from string
+                String content = line.substring(line.indexOf(',') + 1).trim();
 
             }
 
@@ -36,6 +41,6 @@ public class ReadDataset {
         } catch (Exception e) {
             System.out.println("End Of File");
         }
-
+        //read stop words?
     }
 }
